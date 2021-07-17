cd022c43f3fa6293945ffa21fe48349c45feb3ef
Rustam Syubkaev <qva2@ya.ru>
2021-07-17 16:42:23 +0300

    add privacy policy, add banner include, add map include

---
+++ local/include/system/callback_banner.php
@@ -0,0 +1,24 @@
+<div class="banner">
+    <div class="content">
+        <div class="banner-box bg_glitter">
+            <div class="banner-inner">
+                <div class="banner-content">
+                    <h3>Мы открыты для общения</h3>
+                    <p>Директор лично читает все письма и принимает решения по ним</p>
+                    <div class="more-btn">
+                        <a class="btn btn-blue big" href="#" data-toggle="modal" data-target="#modalCall">
+                            <span class="btn__text">
+                                <span data-text="Напишите нам">Напишите нам</span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <a class="logo-link" href="<?= SITE_DIR ?>">
+                    <div class="logo">
+                        <img src="<?= SITE_STYLE_PATH ?>/img/general/logo.svg" alt="<?= SITE_SERVER_NAME ?>">
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ local/include/system/map.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var $arParams
+ */
+?>
+<div class="map">
+    <div class="map__inner">
+        <div id="map" data-coord="<?= json_encode($arParams['ADDRESS']) ?>"></div>
+    </div>
+</div>
