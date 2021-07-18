0e053eda8d9dc49cd0e4eedf3f06a88f35a1af2a
Rustam Syubkaev <qva2@ya.ru>
2021-07-18 20:09:18 +0300

    fix classCatalog, add faq sect and temp

--- local/php_interface/user_class/classCatalog.php
+++ local/php_interface/user_class/classCatalog.php
@@ -2,6 +2,12 @@
 
 namespace Dev;
 
+use Bitrix\Iblock\ElementTable;
+use Bitrix\Iblock\SectionTable;
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\Entity\Query;
+use Bitrix\Main\ObjectPropertyException;
+use Bitrix\Main\SystemException;
 
 /**
  * Класс для работы с каталогом
@@ -10,5 +16,173 @@ namespace Dev;
  */
 class Catalog
 {
+    /**
+     * @param $IBlockID
+     * @param $props
+     * @return array
+     * @throws ArgumentException
+     * @throws ObjectPropertyException
+     * @throws SystemException
+     */
+    public function getElementProps($IBlockID, $props): array
+    {
+        $query = new Query(
+            ElementTable::getEntity()
+        );
+        $query->setGroup([
+            'ID',
+            'PROPERTY_ENUM_VALUE',
+        ]);
+        $query->setFilter([
+            '=IBLOCK_ID' => $IBlockID,
+            '=PROPERTY_CODE' => $props,
+        ]);
+        $query->setSelect([
+            'ID',
+            'NAME',
+            'PROPERTY_CODE' => 'PROPERTY_PROP.CODE',
+            'PROPERTY_VALUE' => 'PROPERTY.VALUE',
+            'PROPERTY_ENUM_VALUE' => 'PROPERTY_ENUM.VALUE',
+        ]);
+        $query->registerRuntimeField(
+            'PROPERTY',
+            [
+                'data_type' => \Bitrix\Iblock\ElementPropertyTable::class,
+                'reference' => ['=this.ID' => 'ref.IBLOCK_ELEMENT_ID'],
+            ]
+        );
+        $query->registerRuntimeField(
+            'PROPERTY_PROP',
+            [
+                'data_type' => \Bitrix\Iblock\PropertyTable::class,
+                'reference' => ['=this.PROPERTY.IBLOCK_PROPERTY_ID' => 'ref.ID',],
+            ]
+        );
+        $query->registerRuntimeField(
+            'PROPERTY_ENUM',
+            [
+                'data_type' => \Bitrix\Iblock\PropertyEnumerationTable::class,
+                'reference' => ['=this.PROPERTY.IBLOCK_PROPERTY_ID' => 'ref.PROPERTY_ID'],
+            ]
+        );
+        \Dev\Utilities::DB($query->getQuery());
+        $result = $query->exec();
+        while ($row = $result->fetch()) {
+            \Dev\Utilities::DB($row);
+        }
+        /*
+        $arResult = ElementTable::getList([
+            'order' => ['SORT' => 'ASC'],
+            'filter' => [
+                '=IBLOCK_ID' => $IBlockID,
+                '=PROPERTY_CODE' => $props,
+            ],
+            'select' => [
+                'ID',
+                'NAME',
+                'PROPERTY_CODE' => 'PROPERTY_PROP.CODE',
+                'PROPERTY_VALUE' => 'PROPERTY.VALUE',
+                'PROPERTY_TYPE' => 'PROPERTY_PROP.PROPERTY_TYPE',
+                'PROPERTY_ENUM_VALUE' => 'PROPERTY_ENUM.VALUE',
+                'PROPERTY_ENUM_XML_ID' => 'PROPERTY_ENUM.XML_ID',
+                'PROPERTY',
+                'PROPERTY_PROP',
+                'PROPERTY_ENUM',
+            ],
+            'group' => [
+                'ID',
+                'PROPERTY_ENUM_VALUE',
+            ],
+            'runtime' => [
+                new \Bitrix\Main\Entity\ReferenceField(
+                    'PROPERTY',
+                    \Bitrix\Iblock\ElementPropertyTable::class,
+                    [
+                        '=this.ID' => 'ref.IBLOCK_ELEMENT_ID',
+                    ],
+                ),
+                new \Bitrix\Main\Entity\ReferenceField(
+                    'PROPERTY_PROP',
+                    \Bitrix\Iblock\PropertyTable::class,
+                    [
+                        '=this.PROPERTY.IBLOCK_PROPERTY_ID' => 'ref.ID',
+                    ],
+                ),
+                new \Bitrix\Main\Entity\ReferenceField(
+                    'PROPERTY_ENUM',
+                    \Bitrix\Iblock\PropertyEnumerationTable::class,
+                    [
+                        '=this.PROPERTY.IBLOCK_PROPERTY_ID' => 'ref.PROPERTY_ID',
+                    ],
+                ),
+            ],
+        ])->fetchAll();
+        \Dev\Utilities::DB($arResult);
+        */
 
+    }
+
+    /**
+     * Получает активные разделы инфоблока с элементами > 0
+     * @param $IBlockID
+     * @return array
+     * @throws ArgumentException
+     * @throws ObjectPropertyException
+     * @throws SystemException
+     */
+    public static function getIBlockSections($IBlockID): array
+    {
+        $query = new Query(
+            SectionTable::getEntity()
+        );
+        $query->setOrder([
+            'SORT' => 'ASC',
+        ]);
+        $query->setFilter([
+            '=IBLOCK_ID' => $IBlockID,
+            'ACTIVE' => 'Y',
+            'GLOBAL_ACTIVE' => 'Y',
+            '>ELEMENT_COUNT' => 0,
+        ]);
+        $query->setSelect([
+            'ID',
+            'NAME',
+            'ELEMENT_COUNT',
+        ]);
+        $query->registerRuntimeField(
+            'ELEMENTS',
+            [
+                'data_type' => ElementTable::class,
+                'reference' => [
+                    '=this.IBLOCK_ID' => 'ref.IBLOCK_ID',
+                    '=this.ID' => 'ref.IBLOCK_SECTION_ID',
+                    '=this.ACTIVE' => 'ref.ACTIVE',
+                ],
+            ]
+        );
+        $query->registerRuntimeField(
+            'ELEMENT_COUNT',
+            [
+                'data_type' => 'integer',
+                'expression' => [
+                    'COUNT(%s)',
+                    'ELEMENTS.NAME',
+                ],
+            ]
+        );
+        return $query->exec()->fetchAll();
+    }
+
+    /**
+     * Получает информацию о разделе по ID
+     * @param $sectionID
+     * @return array|false
+     * @throws ArgumentException
+     * @throws ObjectPropertyException
+     * @throws SystemException
+     */
+    public static function getSectionByID($sectionID)
+    {
+        return SectionTable::GetByID($sectionID)->Fetch();
+    }
 }
---
+++ local/templates/.default/components/bitrix/news.list/faq/template.php
@@ -0,0 +1,81 @@
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<?php if (!empty($arResult['TAGS'])): //TODO: ajax-подгрузка. ?>
+    <div class="tags-wrap">
+        <div class="tags">
+            <a class="tags-item tags-item_active" data-section="" href="javascript:void(0);">
+                <span>Все</span>
+            </a>
+            <?php foreach ($arResult['TAGS'] as $tag): ?>
+                <a class="tags-item" data-section="<?= $tag['ID'] ?>" href="javascript:void(0);">
+                    <span><?= $tag['NAME'] ?></span>
+                </a>
+            <?php endforeach ?>
+        </div>
+    </div>
+<?php endif ?>
+<div class="accordion-wrap">
+    <?php foreach ($arResult["ITEMS"] as $arItem): ?>
+        <?php
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
+        ?>
+        <div class="accordion box" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+            <div class="box__inner">
+                <div class="accordion-title">
+                    <h4><?= $arItem['NAME'] ?></h4>
+                    <?php if (!empty($arItem['IBLOCK_SECTION']['NAME'])): ?>
+                        <span class="label label_small label_marengo">
+                            <?= $arItem['IBLOCK_SECTION']['NAME'] ?>
+                        </span>
+                    <?php endif ?>
+                    <span class="accordion-title__icon">
+                        <svg class="icon__slider-next" width="24" height="24">
+                            <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#slider-next"></use>
+                        </svg>
+                    </span>
+                </div>
+                <?php if (!empty($arItem['PREVIEW_TEXT']) || !empty($arItem['PROPERTIES']['LINKS']['VALUE'])): ?>
+                    <div class="accordion-content">
+                        <div class="accordion-article">
+                            <?= $arItem['~PREVIEW_TEXT'] ?>
+                            <?php if (!empty($arItem['PROPERTIES']['LINKS']['VALUE'])): //TODO: в каком виде ссылки??? ?>
+                                <h5>Ссылки по теме:</h5>
+                            <?php endif ?>
+                        </div>
+                    </div>
+                <?php endif ?>
+            </div>
+        </div>
+    <?php endforeach ?>
+</div>
+<div class="banner-box bg_pippin">
+    <div class="banner-bg">
+        <img class="lazy" src="" data-src="<?= SITE_STYLE_PATH ?>/img/content/banner/5.svg" alt="questions">
+    </div>
+    <div class="banner-inner">
+        <div class="banner-content">
+            <h3>Не нашли ответ</h3>
+            <p>Напишите нам, мы с удовольствием вам поможем</p>
+            <div class="more-btn">
+                <a class="btn btn-red big" href="#" data-toggle="modal" data-target="#modalQuestion">
+                    <span class="btn__text">
+                        <span data-text="Задать вопрос">Задать вопрос</span>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
