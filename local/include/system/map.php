47fb4b2a9aee0957a65439d17e872fe7217f4bb7
Rustam Syubkaev <qva2@ya.ru>
2021-08-15 21:50:58 +0300

    fix map load coordinates

--- local/include/system/map.php
+++ local/include/system/map.php
@@ -3,8 +3,10 @@
  * @var $arParams
  */
 ?>
-<div class="map">
-    <div class="map__inner">
-        <div id="map" data-coord="<?= json_encode($arParams['ADDRESS']) ?>"></div>
+<div class="map-wrap">
+    <div class="map">
+        <div class="map__inner">
+            <div id="mapAddresses" data-center='<?= json_encode($arParams['ADDRESS'][0]) ?>' data-coordinates='<?= json_encode($arParams['ADDRESS'], true) ?>'></div>
+        </div>
     </div>
 </div>
--- local/templates/.default/components/bitrix/news.list/contacts/template.php
+++ local/templates/.default/components/bitrix/news.list/contacts/template.php
@@ -49,7 +49,7 @@ $arAddress = [];
                 <?php
                 $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
                 $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
-                $arAddress[] = $arItem['PROPERTIES']['ADDRESS']['VALUE'];
+                $arAddress[][] = $arItem['PROPERTIES']['ADDRESS']['VALUE'];
                 ?>
                 <div class="contacts-item item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
                     <div class="box">
@@ -127,7 +127,6 @@ $arAddress = [];
                 <h2>Адреса на карте</h2>
             </div>
         </div>
-        <?php //TODO: изменить логику подгрузки карты ?>
         <?php $APPLICATION->IncludeFile(SITE_INCLUDE_PATH . "/system/map.php", ['ADDRESS' => $arAddress], ["SHOW_BORDER" => true]); ?>
     </div>
 </section>
