c44f0d0d29a088c900a1e2e7afe13e17fc624926
Rustam Syubkaev <qva2@ya.ru>
2021-08-19 09:25:59 +0300

    add adv include file

---
+++ local/include/system/adv.php
@@ -0,0 +1,41 @@
+<?php //TODO: Что выводим и откуда ?>
+<div class="adv">
+    <div class="content">
+        <div class="adv-box bg_pippin">
+            <div class="heading">
+                <h3>Выезд специалиста круглосуточно бесплатно</h3>
+            </div>
+            <div class="adv-items items">
+                <div class="adv-item item">
+                    <div class="adv-item__icon">
+                        <img src="<?= SITE_STYLE_PATH ?>/img/content/adv/icon.svg" alt="">
+                    </div>
+                    <p>Приезд специалиста в течение 30 минут</p>
+                </div>
+                <div class="adv-item item">
+                    <div class="adv-item__icon">
+                        <img src="<?= SITE_STYLE_PATH ?>/img/content/adv/icon.svg" alt="">
+                    </div>
+                    <p>Организация похорон под ключ по всем обычаям</p>
+                </div>
+                <div class="adv-item item">
+                    <div class="adv-item__icon">
+                        <img src="<?= SITE_STYLE_PATH ?>/img/content/adv/icon.svg" alt="">
+                    </div>
+                    <p>Работаем круглосуточно и без выходных</p>
+                </div>
+                <div class="adv-item item">
+                    <div class="adv-item__icon">
+                        <img src="<?= SITE_STYLE_PATH ?>/img/content/adv/icon.svg" alt="">
+                    </div>
+                    <p>Работаем в соответствии с ГОСТом</p>
+                </div>
+            </div>
+            <div class="more-btn">
+                <a class="btn btn-red big" href="<?php //TODO: Куда ссылка ?>">
+                    <span class="btn__text"><span data-text="Позвонить">Позвонить</span></span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
