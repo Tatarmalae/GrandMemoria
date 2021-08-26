3a497c8b8ac1ac5b62cfc2fd4ae7bd82aec21a96
Rustam Syubkaev <qva2@ya.ru>
2021-08-26 10:27:22 +0300

    fix address include file

--- local/include/system/address.php
+++ local/include/system/address.php
@@ -16,73 +16,72 @@ try {
 } catch (Throwable $e) {
     Debug::dumpToFile($e->getMessage());
 }
+if (count($arResult['CONTRACTS']) == 0) return false;
 ?>
 
-<?php if (!empty($arResult['CONTRACTS'])): ?>
-    <?php $arAddress = [] ?>
-    <section class="address">
-        <div class="content">
-            <div class="heading">
-                <div class="heading__content">
-                    <h2><?= $arParams['TITLE'] ?></h2>
-                </div>
+<?php $arAddress = [] ?>
+<section class="address">
+    <div class="content">
+        <div class="heading">
+            <div class="heading__content">
+                <h2><?= $arParams['TITLE'] ?></h2>
             </div>
-            <div class="map-wrap">
-                <div class="map-items items">
-                    <div class="swiper-container base-slider" data-count="4">
-                        <div class="slider-wrap swiper-wrapper">
-                            <?php foreach ($arResult['CONTRACTS'] as $key => $arItem): ?>
-                                <?php $arAddress[][] = $arItem['PROPERTIES']['ADDRESS']['VALUE']; ?>
-                                <div class="slider-slide swiper-slide">
-                                    <div class="map-item item">
-                                        <div class="box">
-                                            <?php // TODO: не учли что выводится из контактов и там нет названия Офис №1, телефона ниже и адрес с индексом и длиннее ?>
-                                            <h5>Офис №<?= $key + 1 ?></h5>
-                                            <div class="map-addresses">
+        </div>
+        <div class="map-wrap">
+            <div class="map-items items">
+                <div class="swiper-container base-slider" data-count="4">
+                    <div class="slider-wrap swiper-wrapper">
+                        <?php foreach ($arResult['CONTRACTS'] as $key => $arItem): ?>
+                            <?php $arAddress[][] = $arItem['PROPERTIES']['ADDRESS']['VALUE']; ?>
+                            <div class="slider-slide swiper-slide">
+                                <div class="map-item item">
+                                    <div class="box">
+                                        <?php // TODO: не учли что выводится из контактов и там нет названия Офис №1, телефона ниже и адрес с индексом и длиннее ?>
+                                        <h5>Офис №<?= $key + 1 ?></h5>
+                                        <div class="map-addresses">
+                                            <div class="map-address">
                                                 <div class="map-address">
-                                                    <div class="map-address">
-                                                        <svg class="icon__address" width="14" height="14">
-                                                            <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#address"></use>
-                                                        </svg>
-                                                        <span class="map-address__desc"><?= $arItem['NAME'] ?></span>
-                                                    </div>
+                                                    <svg class="icon__address" width="14" height="14">
+                                                        <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#address"></use>
+                                                    </svg>
+                                                    <span class="map-address__desc"><?= $arItem['NAME'] ?></span>
                                                 </div>
-                                                <?php if (!empty($arItem['PROPERTIES']['PHONE']['VALUE'])): ?>
-                                                    <a class="map-address" href="tel:<?= preg_replace('~\D+~', '', $arItem['PROPERTIES']['PHONE']['VALUE']) ?>">
-                                                        <svg class="icon__phone" width="14" height="14">
-                                                            <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#phone"></use>
-                                                        </svg>
-                                                        <span class="map-address__desc"><?= $arItem['PROPERTIES']['PHONE']['VALUE'] ?></span>
-                                                    </a>
-                                                <?php endif ?>
                                             </div>
+                                            <?php if (!empty($arItem['PROPERTIES']['PHONE']['VALUE'])): ?>
+                                                <a class="map-address" href="tel:<?= preg_replace('~\D+~', '', $arItem['PROPERTIES']['PHONE']['VALUE']) ?>">
+                                                    <svg class="icon__phone" width="14" height="14">
+                                                        <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#phone"></use>
+                                                    </svg>
+                                                    <span class="map-address__desc"><?= $arItem['PROPERTIES']['PHONE']['VALUE'] ?></span>
+                                                </a>
+                                            <?php endif ?>
                                         </div>
                                     </div>
                                 </div>
-                            <?php endforeach ?>
-                        </div>
-                        <div class="slider-arrows slider-arrows_prev">
-                            <div class="slider-btn slider-btn_prev">
-                                <svg class="icon__slider-prev" width="32" height="32">
-                                    <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#slider-prev"></use>
-                                </svg>
                             </div>
+                        <?php endforeach ?>
+                    </div>
+                    <div class="slider-arrows slider-arrows_prev">
+                        <div class="slider-btn slider-btn_prev">
+                            <svg class="icon__slider-prev" width="32" height="32">
+                                <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#slider-prev"></use>
+                            </svg>
                         </div>
-                        <div class="slider-arrows slider-arrows_next">
-                            <div class="slider-btn slider-btn_next">
-                                <svg class="icon__slider-next" width="32" height="32">
-                                    <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#slider-next"></use>
-                                </svg>
-                            </div>
+                    </div>
+                    <div class="slider-arrows slider-arrows_next">
+                        <div class="slider-btn slider-btn_next">
+                            <svg class="icon__slider-next" width="32" height="32">
+                                <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#slider-next"></use>
+                            </svg>
                         </div>
                     </div>
                 </div>
-                <?php $APPLICATION->IncludeFile(SITE_INCLUDE_PATH . "/system/map.php", [
-                    'ID' => 'map',
-                    'ADDRESS' => $arAddress,
-                ], ["SHOW_BORDER" => true]); ?>
-                <?php unset($arAddress) ?>
             </div>
+            <?php $APPLICATION->IncludeFile(SITE_INCLUDE_PATH . "/system/map.php", [
+                'ID' => 'map',
+                'ADDRESS' => $arAddress,
+            ], ["SHOW_BORDER" => true]); ?>
+            <?php unset($arAddress) ?>
         </div>
-    </section>
-<?php endif ?>
+    </div>
+</section>
