ef705dc3b0a2e4fdb2f898e4955c80d3dce01885
Rustam Syubkaev <qva2@ya.ru>
2021-08-28 11:38:44 +0300

    include file installment plan, delete unused css

--- local/templates/.default/components/bitrix/news.list/installment/template.php
+++ local/templates/.default/components/bitrix/news.list/installment/template.php
@@ -60,29 +60,7 @@ $this->setFrameMode(true);
     </section>
 <?php endif ?>
 
-<div class="banner">
-    <div class="content">
-        <div class="banner-box bg_pippin">
-            <div class="banner-bg">
-                <img class="lazy" data-src="<?= SITE_STYLE_PATH ?>/img/content/banner/1.svg" alt="" src="">
-            </div>
-            <div class="banner-inner">
-                <div class="banner-content">
-                    <h3>Расчет рассрочки на приобретение памятника</h3>
-                    <p>Для вашего удобства мы предлагаем рассчитать рассрочку на срок от 2 до 12 месяцев</p>
-                    <div class="more-btn">
-                        <a class="btn btn-red big" href="#" data-toggle="modal" data-target="#modalCommunication">
-                            <?php //TODO: какая модалка должна открываться ?>
-                            <span class="btn__text">
-                                <span data-text="Оставить заявку">Оставить заявку</span>
-                            </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<?php $APPLICATION->IncludeFile(SITE_INCLUDE_PATH . "/system/installment_banner.php", [], ["SHOW_BORDER" => true]); ?>
 
 <?php $APPLICATION->IncludeFile(SITE_INCLUDE_PATH . "/system/calculator_installment.php", [], ["SHOW_BORDER" => true]); ?>
 
