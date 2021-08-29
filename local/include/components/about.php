a50a6417a91c9cd02fad3876337bf7f3e7466f3a
Rustam Syubkaev <qva2@ya.ru>
2021-08-29 17:44:33 +0300

    add about include component

---
+++ local/include/components/about.php
@@ -0,0 +1,57 @@
+<?php
+
+use Bitrix\Main\Diag\Debug;
+use Dev\Catalog;
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * @var $arResult
+ * @var $arParams
+ * @var $APPLICATION
+ */
+
+try {
+    $arResult['IBLOCK'] = Catalog::getIBlock(9);
+    $arResult['ITEMS'] = Catalog::getElementList(9);
+} catch (Throwable $e) {
+    Debug::dumpToFile($e->getMessage());
+}
+if (count($arResult['ITEMS']) == 0) return;
+?>
+<div class="adv-box bg_pippin">
+    <div class="adv-info items">
+        <div class="adv-info__column item">
+            <div class="adv-info__left">
+                <a class="logo-link" href="<?= SITE_DIR ?>">
+                    <div class="logo">
+                        <img src="<?= SITE_STYLE_PATH ?>/img/general/logo.svg" alt="<?= SITE_SERVER_NAME ?>"/>
+                    </div>
+                </a>
+                <div class="more">
+                    <a class="more__link" href="/info/about/">
+                        <span>Подробнее</span>
+                        <svg class="icon__arrow-right" width="24" height="24">
+                            <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#arrow-right"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="adv-info__column item">
+            <h3><?= $arResult['IBLOCK']['NAME'] ?></h3>
+            <p>
+                <?= $arResult['IBLOCK']['DESCRIPTION'] ?>
+            </p>
+            <div class="adv-counts items">
+                <?php foreach ($arResult['ITEMS'] as $arItem): ?>
+                    <?php if (!$arItem['PROPERTIES']['SHOW_INDEX']['VALUE']) continue ?>
+                    <div class="adv-counts__item item">
+                        <span class="adv-counts__count"><?= $arItem['DETAIL_TEXT'] ?></span>
+                        <span class="adv-counts__desc"><?= $arItem['NAME'] ?></span>
+                    </div>
+                <?php endforeach ?>
+            </div>
+        </div>
+    </div>
+</div>
