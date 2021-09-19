6f2f6e336e293c28456d82789ab83a98faaf5bca
Rustam Syubkaev <qva2@ya.ru>
2021-09-19 17:55:59 +0300

    fix price list template

--- local/templates/.default/components/bitrix/news.list/price/template.php
+++ local/templates/.default/components/bitrix/news.list/price/template.php
@@ -30,44 +30,42 @@ $this->setFrameMode(true);
     <?php $this->EndViewTarget(); ?>
 <?php endif ?>
 
-<div class="content">
-    <div class="tab-content" id="priceTabContent">
-        <?php foreach ($arResult['TAGS'] as $key => $tag): ?>
-            <div class="tab-pane fade<?= $key == 0 ? ' show active' : '' ?>" id="tab<?= $key + 1 ?>" role="tabpanel" aria-labelledby="tab-<?= $key + 1 ?>">
-                <div class="tariffs-items items">
-                    <?php foreach ($arResult["ITEMS"] as $arItem): ?>
-                        <?php
-                        if ($arItem['IBLOCK_SECTION_ID'] !== $tag['ID']) continue;
-                        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-                        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
-                        ?>
-                        <div class="tariffs-item item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-                            <div class="box">
-                                <h4><?= $arItem['NAME'] ?></h4>
-                                <?php if (!empty($arItem['PREVIEW_TEXT'])): ?>
-                                    <span class="tariffs-item__price">
-                                        <?= $arItem['PREVIEW_TEXT'] ?>
-                                    </span>
-                                <?php endif ?>
-                                <?php if (!empty($arItem['DETAIL_TEXT'])): ?>
-                                    <div class="tariffs-item__content">
-                                        <?= $arItem['DETAIL_TEXT'] ?>
-                                    </div>
-                                <?php endif ?>
-                                <div class="more-btn">
-                                    <a class="btn btn-blue big btn-block" href="#" data-toggle="modal" data-target="#modalCheckout"<?php //TODO: передать id услуги ?>>
-                                        <span class="btn__text">
-                                            <span data-text="Заказать">Заказать</span>
-                                        </span>
-                                    </a>
+<div class="tab-content" id="priceTabContent">
+    <?php foreach ($arResult['TAGS'] as $key => $tag): ?>
+        <div class="tab-pane fade<?= $key == 0 ? ' show active' : '' ?>" id="tab<?= $key + 1 ?>" role="tabpanel" aria-labelledby="tab-<?= $key + 1 ?>">
+            <div class="tariffs-items items">
+                <?php foreach ($arResult["ITEMS"] as $arItem): ?>
+                    <?php
+                    if ($arItem['IBLOCK_SECTION_ID'] !== $tag['ID']) continue;
+                    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
+                    ?>
+                    <div class="tariffs-item item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+                        <div class="box">
+                            <h4><?= $arItem['NAME'] ?></h4>
+                            <?php if (!empty($arItem['PREVIEW_TEXT'])): ?>
+                                <span class="tariffs-item__price">
+                                    <?= $arItem['PREVIEW_TEXT'] ?>
+                                </span>
+                            <?php endif ?>
+                            <?php if (!empty($arItem['DETAIL_TEXT'])): ?>
+                                <div class="tariffs-item__content">
+                                    <?= $arItem['DETAIL_TEXT'] ?>
                                 </div>
+                            <?php endif ?>
+                            <div class="more-btn">
+                                <a class="btn btn-blue big btn-block" href="#" data-toggle="modal" data-target="#modalCheckout"<?php //TODO: передать id услуги ?>>
+                                    <span class="btn__text">
+                                        <span data-text="Заказать">Заказать</span>
+                                    </span>
+                                </a>
                             </div>
                         </div>
-                    <?php endforeach ?>
-                </div>
+                    </div>
+                <?php endforeach ?>
             </div>
-        <?php endforeach ?>
-    </div>
+        </div>
+    <?php endforeach ?>
 </div>
 
 <?php $APPLICATION->IncludeFile(SITE_INCLUDE_PATH . "/system/feedback_form.php", [], ["SHOW_BORDER" => true]); ?>
