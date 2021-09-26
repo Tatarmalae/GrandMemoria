a495bd1dfb4500be855b31152c6a202c5f1b8b64
Rustam Syubkaev <qva2@ya.ru>
2021-09-26 21:04:14 +0300

    fix article list template

--- local/templates/.default/components/bitrix/news/articles/bitrix/news.list/.default/template.php
+++ local/templates/.default/components/bitrix/news/articles/bitrix/news.list/.default/template.php
@@ -31,40 +31,99 @@ $this->setFrameMode(true);
         <?php
         $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
         $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
+        $class = '';
+        if ($key === 0) {
+            $class .= ' articles-item_big';
+            if (!empty($arItem['PREVIEW_PICTURE']['SRC'])) {
+                $class .= ' white';
+            }
+        }
         ?>
-        <div class="articles-item item<?= $key === 0 ? ' articles-item_big' : ' link-item' ?>" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-            <div class="<?= $key === 0 ? 'articles-big' : (!empty($arItem['PREVIEW_PICTURE']['SRC']) ? 'box' : 'articles-info') ?>">
+        <a href="<?= $arItem['DETAIL_PAGE_URL'] ?>"
+           class="articles-item item link-item<?= $class ?>"
+           id="<?= $this->GetEditAreaId($arItem['ID']); ?>"
+           data-img="<?= empty($arItem['PREVIEW_PICTURE']['SRC']) ? 'not' : '' ?>"
+        >
+            <?php if ($key === 0): ?>
+                <div class="articles-big">
+                    <?php if (!empty($arItem['PREVIEW_PICTURE']['SRC'])): ?>
+                        <div class="articles-item__img img">
+                            <div class="img__inner object-fit">
+                                <img src="" class="lazy" data-src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $arItem['NAME'] ?>">
+                            </div>
+                        </div>
+                    <?php else: ?>
+                        <div class="articles-bg">
+                            <img src="" class="lazy" data-src="<?= SITE_STYLE_PATH ?>/img/content/articles/bg.svg" alt="bg">
+                        </div>
+                    <?php endif ?>
+                    <div class="articles-item__content">
+                        <?php if (!empty($arItem['IBLOCK_SECTION']['NAME'])): ?>
+                            <span class="label label_small label_marengo">
+                                <?= $arItem['IBLOCK_SECTION']['NAME'] ?>
+                            </span>
+                        <?php endif ?>
+                        <h3><?= $arItem['NAME'] ?></h3>
+                        <div class="more">
+                            <span class="more__link link-static">
+                                <span>Подробнее</span>
+                                <svg class="icon__arrow-right" width="24" height="24">
+                                    <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#arrow-right"></use>
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            <?php else: ?>
                 <?php if (!empty($arItem['PREVIEW_PICTURE']['SRC'])): ?>
-                    <div class="articles-item__img img">
-                        <div class="img__inner object-fit">
-                            <img src="" class="lazy" data-src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $arItem['NAME'] ?>">
+                    <div class="box">
+                        <div class="articles-item__img img">
+                            <div class="img__inner object-fit">
+                                <img src="" class="lazy" data-src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $arItem['NAME'] ?>">
+                            </div>
+                        </div>
+                        <div class="articles-item__content">
+                            <?php if (!empty($arItem['IBLOCK_SECTION']['NAME'])): ?>
+                                <span class="label label_small label_marengo">
+                                    <?= $arItem['IBLOCK_SECTION']['NAME'] ?>
+                                </span>
+                            <?php endif ?>
+                            <h4><?= $arItem['NAME'] ?></h4>
+                            <div class="more">
+                                <span class="more__link link-static">
+                                    <span>Подробнее</span>
+                                    <svg class="icon__arrow-right" width="24" height="24">
+                                        <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#arrow-right"></use>
+                                    </svg>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 <?php else: ?>
-                    <div class="articles-bg">
-                        <img src="" class="lazy" data-src="<?= SITE_STYLE_PATH ?>/img/content/articles/bg.svg" alt="bg">
+                    <div class="articles-info">
+                        <div class="articles-bg">
+                            <img src="" class="lazy" data-src="<?= SITE_STYLE_PATH ?>/img/content/articles/bg.svg" alt="bg">
+                        </div>
+                        <div class="articles-item__content">
+                            <?php if (!empty($arItem['IBLOCK_SECTION']['NAME'])): ?>
+                                <span class="label label_small label_marengo">
+                                    <?= $arItem['IBLOCK_SECTION']['NAME'] ?>
+                                </span>
+                            <?php endif ?>
+                            <h3><?= $arItem['NAME'] ?></h3>
+                            <div class="more">
+                                <span class="more__link link-static">
+                                    <span>Подробнее</span>
+                                    <svg class="icon__arrow-right" width="24" height="24">
+                                        <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#arrow-right"></use>
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 <?php endif ?>
-                <div class="articles-item__content">
-                    <?php if (!empty($arItem['IBLOCK_SECTION']['NAME'])): ?>
-                        <span class="label label_small label_marengo">
-                            <?= $arItem['IBLOCK_SECTION']['NAME'] ?>
-                        </span>
-                    <?php endif ?>
-                    <?= ($key === 0) ? '<h3>' : '<h4>' ?>
-                    <?= $arItem['NAME'] ?>
-                    <?= ($key === 0) ? '</h3>' : '</h4>' ?>
-                    <div class="more">
-                        <a class="more__link<?= ($key !== 0) ? ' link-static' : '' ?>" href="<?= $arItem['DETAIL_PAGE_URL'] ?>">
-                            <span>Подробнее</span>
-                            <svg class="icon__arrow-right" width="24" height="24">
-                                <use xlink:href="<?= SITE_STYLE_PATH ?>/img/general/svg-symbols.svg#arrow-right"></use>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+            <?php endif ?>
+        </a>
     <?php endforeach; ?>
 </div>
 <?php if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
