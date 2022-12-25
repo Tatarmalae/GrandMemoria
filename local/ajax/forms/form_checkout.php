a1589a006c7ca62ea469654c9beb0ecc64fe0db6
Rustam Syubkaev <qva2@ya.ru>
2022-12-25 09:49:15 +0300

    add Google reCAPTCHA_v3

--- local/ajax/forms/form_checkout.php
+++ local/ajax/forms/form_checkout.php
@@ -4,18 +4,29 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_be
 use Bitrix\Main\Diag\Debug;
 use Dev\FormCheckout;
 
-$obForm = new FormCheckout(33);
+$reCaptcha = (new Dev\ReCaptcha)->getData($_POST['g-recaptcha-response']);
+if (!$reCaptcha['success'] || $reCaptcha['action'] !== 'send_form' || floatval($reCaptcha['score']) < 0.5) {
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Captcha Error',
+    ]);
+} else {
+    $obForm = new FormCheckout(33);
 
-$arFields = $_POST;
-$type = $arFields['type'];
-unset($arFields['type'], $arFields['checkbox']);
-try {
-    $res = $obForm->add($type, $arFields);
-    if ($res === true) {
-        echo json_encode(['status' => 'ok', 'reload' => 'ok']);
-    } else {
-        Debug::dumpToFile($res);
+    $arFields = $_POST;
+    $type = $arFields['type'];
+    unset($arFields['type'], $arFields['checkbox']);
+    try {
+        $res = $obForm->add($type, $arFields);
+        if ($res === true) {
+            echo json_encode([
+                'status' => 'ok',
+                'reload' => 'ok',
+            ]);
+        } else {
+            Debug::dumpToFile($res);
+        }
+    } catch (Throwable $e) {
+        Debug::dumpToFile($e->getMessage());
     }
-} catch (Throwable $e) {
-    Debug::dumpToFile($e->getMessage());
 }
